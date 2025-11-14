880dfc653f7650c9d5b459aac395fc25d5d716b2
juliethron <juliethron@gmail.com>
2025-11-14 20:49:50 +0100

    Update product.js

--- product.js
+++ product.js
@@ -1,4 +1,3 @@
-
 import { movies } from "./home.js"; 
 
 const container = document.querySelector("#movie-details");
@@ -8,25 +7,24 @@ const id = params.get("id");
 const movie = movies.find((item) => item.id === id);
 
 if (!movie) {
-    container.innerHTML = `<p>Movie not found.</p>`;
+    container.innerHTML = `<p class="error">Movie not found.</p>`;
 } else {
     container.innerHTML = `
-    <img src="${movie.image}" alt="${movie.title}">
-    <h1>${movie.title}</h1>
-    <p><strong>Genre:</strong> ${movie.genre}</p>
-    <p>${movie.description}</p>
-    <p class="price"><strong>Price:</strong> $${movie.price.toFixed(2)}</p>
-    <button id="addToCart" class="btn">Add to Basket</button>
+        <img src="${movie.image}" alt="${movie.title}">
+        <h1>${movie.title}</h1>
+        <p><strong>Genre:</strong> ${movie.genre}</p>
+        <p>${movie.description}</p>
+        <p class="price"><strong>Price:</strong> $${movie.price.toFixed(2)}</p>
+        <button id="addToCart" class="btn">Add to Basket</button>
     `;
 
-    document
-    .querySelector("#addToCart")
-    .addEventListener("click", () => addToCart(movie));
+    document.querySelector("#addToCart").addEventListener("click", () => addToCart(movie));
 }
 
 function addToCart(movie) {
     const cart = JSON.parse(localStorage.getItem("cart")) || [];
     cart.push(movie);
     localStorage.setItem("cart", JSON.stringify(cart));
-    alert(`${movie.title} added to your basket!`);
+    
+    alert(`"${movie.title}" has been added to your basket!`);
 }
