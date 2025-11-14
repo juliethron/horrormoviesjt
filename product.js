3776fe13491dc85bb315c75599165608b47d2f9b
juliethron <juliethron@gmail.com>
2025-11-14 20:54:15 +0100

    Refactor movie details rendering and fix image access

--- product.js
+++ product.js
@@ -1,16 +1,16 @@
-import { movies } from "./home.js"; 
+import { movies } from "./home.js";
 
 const container = document.querySelector("#movie-details");
 const params = new URLSearchParams(window.location.search);
 const id = params.get("id");
 
-const movie = movies.find((item) => item.id === id);
+const movie = movies.find(item => item.id === id);
 
 if (!movie) {
     container.innerHTML = `<p class="error">Movie not found.</p>`;
 } else {
     container.innerHTML = `
-        <img src="${movie.image}" alt="${movie.title}">
+        <img src="${movie.image.url}" alt="${movie.image.alt}">
         <h1>${movie.title}</h1>
         <p><strong>Genre:</strong> ${movie.genre}</p>
         <p>${movie.description}</p>
@@ -25,6 +25,5 @@ function addToCart(movie) {
     const cart = JSON.parse(localStorage.getItem("cart")) || [];
     cart.push(movie);
     localStorage.setItem("cart", JSON.stringify(cart));
-    
     alert(`"${movie.title}" has been added to your basket!`);
 }
